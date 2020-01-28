3723ae4ea72b7bc6d6a35ff2aa39beb29f263ff8
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-01-29 02:51:57 +0300

    correctly setup redux and redux(using dispatch and useselector to access state)

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import MainApp from "./screens/mainApp";
 import { mainReducer } from "./store/reducers";
+
+/* if we had more than one reducer we use combine reducers like below, then pass root
+reducer to createstore. From our components we can then access what we 
+want using state.reducername.whatwewant e.g state.main.counter for below */
+
+// const rootReducer = combineReducers({
+//   main: mainReducer
+// });
+// const store = createStore(rootReducer);
 
 const store = createStore(mainReducer);
 
--- screens/mainApp.js
+++ screens/mainApp.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { increaseCounter, decreaseCounter } from "../store/actions";
 
-const MainApp = props => {
-  const addition = counter => {
-    props.dispatch(increaseCounter(counter));
-  };
-  const subtraction = counter => {
-    props.dispatch(decreaseCounter(counter));
-  };
-  const { counter } = props; //instead of props.counter just destructure it
+const MainApp = () => {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
-      <Button title="Increase" onPress={() => addition(counter)} />
-      {/* withount destructuring we would call props.counter, after mapstatetoprops */}
+      <Button
+        title="Increase"
+        onPress={() => dispatch(increaseCounter(counter))}
+      />
       <Text>{counter}</Text>
-      <Button title="Decrease" onPress={()=>subtraction(counter)} />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch(decreaseCounter(counter))}
+      />
     </View>
   );
-};
-
-const mapStateToProps = state => {
-  return {
-    counter: state.counter
-  };
 };
 
 const styles = StyleSheet.create({
@@ -36,6 +31,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default connect(mapStateToProps)(
-  MainApp
-);
+export default MainApp;
